16ebe5f3a009b9d99c101727f421911aa9a1567e
om-jasoliya91 <patoliya.oj@gmail.com>
2025-10-06 12:41:38 +0530

    pdf

--- app/Libraries/PdfGenerator.php
+++ app/Libraries/PdfGenerator.php
@@ -4,21 +4,19 @@ namespace App\Libraries;
 
 class PdfGenerator
 {
- public function generate($html, $paperSize = 'A4', $orientation = 'portrait', $stream = false, $filename = 'document.pdf')
-{
-    $dompdf = new \Dompdf\Dompdf();
-    $dompdf->loadHtml($html);
-    $dompdf->setPaper($paperSize, $orientation);
-    $dompdf->render();
+    public function generate($html, $paperSize = 'A4', $orientation = 'portrait', $stream = false, $filename = 'document.pdf')
+    {
+        $dompdf = new \Dompdf\Dompdf();
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper($paperSize, $orientation);
+        $dompdf->render();
 
-    if ($stream) {
-        // Stream to browser, inline display
-        $dompdf->stream($filename, ['Attachment' => 0]);
-        return null;
+        if ($stream) {
+            // Stream to browser, inline display
+            $dompdf->stream($filename, ['Attachment' => 0]);
+            return null;
+        }
+
+        return $dompdf->output();
     }
-
-    return $dompdf->output();
-}
-
-
 }
